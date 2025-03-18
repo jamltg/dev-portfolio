86c77f005dc923fbfd2f399ae3be5e9dac3fc33f
jamltg <jamletigio@gmail.com>
2025-03-19 01:00:27 +0800

    changed NavBar to navbar in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Projects from "./components/Projects"
-import { NavigationBar } from "./components/NavBar"
+import { NavigationBar } from "./components/Navbar"
 import About from "./components/About"
 import Collaborate from "./components/Collaborate"
 import Contact from "./components/Contact"
