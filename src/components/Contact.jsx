b394a4a222e45131e3c72d0a196ec968e8d33cb5
jamltg <jamletigio@gmail.com>
2025-03-19 01:04:13 +0800

    added contact id for navbar

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import ContactInfo from './ContactInfo'
 
 export default function Contact() {
   return (
-    <div className='bg-[#EDE7DE] pt-14 pb-28'>
+    <div id='contact' className='bg-[#EDE7DE] pt-14 pb-28'>
         <div className='max-w-7xl mx-auto px-10'>
             <ContactInfo/>
         </div>
